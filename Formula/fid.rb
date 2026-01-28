1de566b3fef22c9467e264d488ee1681563cc2ef
3000-2 <psch0493@gmail.com>
2026-01-28 22:04:26 +0900

    chore: bump fid to v0.1.1, fix dependency install

--- Formula/fid.rb
+++ Formula/fid.rb
@@ -1,13 +1,14 @@
 class Fid < Formula
   desc "Terminal UI for viewing git diffs with style"
   homepage "https://github.com/3000-2/fid"
-  url "https://github.com/3000-2/fid/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4ffb925eddf8a9911355727983303a6a9baac270afc9358da147125b5d43676c"
+  url "https://github.com/3000-2/fid/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "f9a6e7b57dc76687abc935e85f06848ad151c6de1e11da3a8e085b77f1293e65"
   license "MIT"
 
   depends_on "oven-sh/bun/bun"
 
   def install
+    system "bun", "install"
     libexec.install Dir["*"]
     (bin/"fid").write <<~EOS
       #!/bin/bash
