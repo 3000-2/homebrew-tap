b5fa534784b01bb78de9b7733994915eb1e4c260
3000-2 <psch0493@gmail.com>
2026-01-29 14:49:34 +0900

    Update fid to v0.2.0

--- Formula/fid.rb
+++ Formula/fid.rb
@@ -1,8 +1,8 @@
 class Fid < Formula
   desc "Terminal UI for viewing git diffs with style"
   homepage "https://github.com/3000-2/fid"
-  url "https://github.com/3000-2/fid/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "0abdd8c113e6099c09df199a9cc337ca2e2badde839bb6d9a31c065836effd37"
+  url "https://github.com/3000-2/fid/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "d0a1fda603c69df2d3644305f7456b2529f2d36c32d56436903413eea5829a06"
   license "MIT"
 
   depends_on "oven-sh/bun/bun"
